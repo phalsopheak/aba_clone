6fd435d2b7e30be9daecadc827e045d06dcd7a5f
phalsopheak <sopheak.programming@gmail.com>
2021-03-10 22:30:37 +0700

    add test datasource

---
+++ lib/data/data_source/data_test.dart
@@ -0,0 +1,35 @@
+import 'package:aba_clone/data/models/notification_transaction_model.dart';
+
+class DataTest {
+  const DataTest();
+
+  List<NotificationTransactionModel> listNotificationTransaction() {
+    var list = List<NotificationTransactionModel>();
+    list = [
+      NotificationTransactionModel(
+        iconPath: '',
+        title: '',
+        dateTime: '',
+        description: '',
+      ),
+      NotificationTransactionModel(
+        iconPath: '',
+        title: '',
+        dateTime: '',
+        description: '',
+      ),
+      NotificationTransactionModel(
+        iconPath: '',
+        title: '',
+        dateTime: '',
+        description: '',
+      ),
+      NotificationTransactionModel(
+        iconPath: '',
+        title: '',
+        dateTime: '',
+        description: '',
+      ),
+    ];
+  }
+}
---
+++ lib/data/models/notification_transaction_model.dart
@@ -0,0 +1,13 @@
+class NotificationTransactionModel {
+  final String iconPath;
+  final String title;
+  final String dateTime;
+  final String description;
+
+  NotificationTransactionModel({
+    this.iconPath,
+    this.title,
+    this.dateTime,
+    this.description,
+  });
+}
